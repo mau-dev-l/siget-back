1c6511d98b0eb8410d7695643e9ef526f709ea74
Sergio Mauricio Rivera Lara <mauri090604@hotmail.com>
2026-02-26 19:02:10 -0600

    login 2.0

--- models/user_model.py
+++ models/user_model.py
@@ -1,5 +1,6 @@
 from db.connection import execute_read_query
 
 def get_user_by_username(username: str):
-    query= "SELECT id, username, password, role FROM users WHERE username = %s"
+    query= "SELECT id, username, password, role FROM users WHERE username = %(username)s"
     return execute_read_query(query, params={"username": username}, use_pool2=True) 
+
